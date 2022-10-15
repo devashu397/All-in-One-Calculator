7425065f62e34d240768b7d35624f0b62e802370
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2022-10-16 00:28:53 +0530

    Delete misc.js

--- js/misc.js
+++
@@ -1,6 +0,0 @@
-function roundnumber() {
-  const roundnumber = document.getElementById("roundnumberinput").value;
-  const resultdiv = document.getElementById("resultdiv");
-
-  resultdiv.innerHTML = `Rounded Number - ${Math.round(roundnumber)}`;
-}
